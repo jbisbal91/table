e494beb05d912b953fb7d35bbc93e65f5a4a9f0c
Jose A Bisbal <jbisbal91@gmail.com>
2022-06-24 11:46:39 -0400

    table-edit is create

--- src/app/app-shared/components/table-edit/table-edit.component.ts
+++ src/app/app-shared/components/table-edit/table-edit.component.ts
@@ -10,34 +10,70 @@ export class TableEditComponent implements OnInit {
   displayedColumns: string[] = [];
   columns: string[] = [];
   public dataSource: any;
+
+  editMode = false;
+  editableIndex:number = -1;
+  clickedRow = -1;
+  selectedColumn = 1;
+  rowToUpdate = null;
+
+  cid = 0;
+  name = '';
+  surname = '';
+  address = '';
+  sexo = '';
   constructor() { }
 
 
   users = [
-    {name:'Jose A',surname:'Bisbal',cid:910,address:'Santa Clata'},
-    {name:'Jose A',surname:'Bisbal',cid:910,address:'Santa Clata'},
-    {name:'Jose A',surname:'Bisbal',cid:910,address:'Santa Clata'},
-    {name:'Jose A',surname:'Bisbal',cid:910,address:'Santa Clata'},
-    {name:'Jose A',surname:'Bisbal',cid:910,address:'Santa Clata'},
-    {name:'Jose A',surname:'Bisbal',cid:910,address:'Santa Clata'},
-    {name:'Jose A',surname:'Bisbal',cid:910,address:'Santa Clata'},
-    {name:'Jose A',surname:'Bisbal',cid:910,address:'Santa Clata'},
-    {name:'Jose A',surname:'Bisbal',cid:910,address:'Santa Clata'},
-    {name:'Jose A',surname:'Bisbal',cid:910,address:'Santa Clata'},
-    {name:'Jose A',surname:'Bisbal',cid:910,address:'Santa Clata'},
-    {name:'Jose A',surname:'Bisbal',cid:910,address:'Santa Clata'},
-    {name:'Jose A',surname:'Bisbal',cid:910,address:'Santa Clata'},
-    {name:'Jose A',surname:'Bisbal',cid:910,address:'Santa Clata'},
-    {name:'Jose A',surname:'Bisbal',cid:910,address:'Santa Clata'},
-    {name:'Jose A',surname:'Bisbal',cid:910,address:'Santa Clata'},
-    {name:'Jose A',surname:'Bisbal',cid:910,address:'Santa Clata'},
-    {name:'Jose A',surname:'Bisbal',cid:910,address:'Santa Clata'},
-    {name:'Jose A',surname:'Bisbal',cid:910,address:'Santa Clata'},
-    {name:'Jose A',surname:'Bisbal',cid:910,address:'Santa Clata'}
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:12,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:34,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:454,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:232,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:9564510,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:45,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:945410,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:945410,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'},
+    {cid:910,name:'Jose A',surname:'Bisbal',address:'Santa Clata',sexo:'M'}
   ]
 
   getDataConf() {
     return [
+        {
+            prop: 'cid',
+            name: 'ID number'
+        },
         {
             prop: 'name',
             name: 'Name'
@@ -47,12 +83,12 @@ export class TableEditComponent implements OnInit {
             name: 'Surname'
         },
         {
-            prop: 'cid',
-            name: 'ID number'
-        },
-        {
             prop: 'address',
             name: 'Address'
+        },
+        {
+            prop: 'sexo',
+            name: 'Sexo'
         }
     ];  
   }
@@ -63,5 +99,29 @@ export class TableEditComponent implements OnInit {
     this.dataSource = new MatTableDataSource<any>(this.users);
   }
 
+  showInput(index: number, column: number) {
+    return this.editableIndex === index && this.selectedColumn === column;
+  }
+
+  disableEditMode() {
+    this.editMode = false;
+    this.editableIndex = -1;
+    this.clickedRow = -1;
+    this.selectedColumn = -1;
+    this.rowToUpdate = null;
+  }
+
+  edit(row: any, index: number, column: number) {
+    this.disableEditMode();
+    this.cid = row.cid;
+    this.name = row.name;
+    this.surname = row.surname;
+    this.address = row.address;
+    this.sexo = row.sexo;
+    this.editMode = true;
+    this.editableIndex = index;
+    this.selectedColumn = column;
+    this.rowToUpdate = row;
+  }
 
 }
